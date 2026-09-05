89933260ad2f8e7d9248a278c41dbbdb8d275be4
islammdsohan603 <islammdsohan603@gmail.com>
2026-09-05 22:35:31 +0600

    maltupaul

--- frist.c
+++ frist.c
@@ -1,17 +1,13 @@
-// sum of two numbers
-
 #include <stdio.h>
 
-int main(){
+int main()
+{
+     int a,b,sum;
+    printf("Enter two integers: ");
+    scanf("%d %d",&a,&b);
 
-    int a;
-
-    scanf("%d", &a);
-
-    printf("You entered: %d\n", a);
+    sum = a + b;
+    printf("%d * %d = %d",a,b,sum);
 
     return 0;
 }
-
-
-    
